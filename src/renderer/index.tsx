c6d97bc462da316f7503ab0990497872971096a3
Radium <2043869032@qq.com>
2021-12-01 22:50:04 +0800

    commit

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,4 +1,5 @@
 import { render } from 'react-dom';
-import App from './App';
+import App from './pages/App';
+import '@arco-design/web-react/dist/css/arco.css';
 
 render(<App />, document.getElementById('root'));
---
+++ src/renderer/pages/App.tsx
@@ -0,0 +1,5 @@
+import { Button } from '@arco-design/web-react';
+
+export default function App() {
+  return <Button type="primary">Hello</Button>;
+}
