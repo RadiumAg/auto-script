28d80e342acb4ec73864e4a6fd090819506bc176
2043869032@qq.com <2043869032@qq.com>
2022-07-09 18:40:22 +0800

    commit

--- src/main/auto-script/scripts/TicktokCross.ts
+++ src/main/auto-script/scripts/TicktokCross.ts
@@ -9,11 +9,13 @@ enum EShop {
   菲律宾 = '菲律宾',
   泰国 = '泰国',
   越南 = '越南',
+  英国 = '英国',
+  新加坡 = '新加坡',
 }
 
 export class TickTokCross extends Run {
   protected async run(key: string, message: string, shop: EShop) {
-    await this.driver.sleep(this.waitTime * 2);
+    await this.driver.sleep(8000);
     // 过导航（非必选）
     try {
       await this.driver
@@ -35,7 +37,7 @@ export class TickTokCross extends Run {
         .click();
 
       switch (shop) {
-        case EShop.马来西亚:
+        case EShop.英国:
           await this.driver
             .findElement(
               By.css(
@@ -45,7 +47,7 @@ export class TickTokCross extends Run {
             .click();
           break;
 
-        case EShop.菲律宾:
+        case EShop.马来西亚:
           await this.driver
             .findElement(
               By.css(
@@ -55,7 +57,7 @@ export class TickTokCross extends Run {
             .click();
           break;
 
-        case EShop.泰国:
+        case EShop.菲律宾:
           await this.driver
             .findElement(
               By.css(
@@ -65,7 +67,7 @@ export class TickTokCross extends Run {
             .click();
           break;
 
-        case EShop.越南:
+        case EShop.新加坡:
           await this.driver
             .findElement(
               By.css(
@@ -75,10 +77,30 @@ export class TickTokCross extends Run {
             .click();
           break;
 
+        case EShop.泰国:
+          await this.driver
+            .findElement(
+              By.css(
+                '#root > div > div.layout__navbar--JaN0B > div > div:nth-child(4) > span > div.zep-popover-content.zep-popover-content-bottom > div > div > div > div > div > div.self-start.px-24 > span > label:nth-child(5)',
+              ),
+            )
+            .click();
+          break;
+
+        case EShop.越南:
+          await this.driver
+            .findElement(
+              By.css(
+                '#root > div > div.layout__navbar--JaN0B > div > div:nth-child(4) > span > div.zep-popover-content.zep-popover-content-bottom > div > div > div > div > div > div.self-start.px-24 > span > label:nth-child(6)',
+              ),
+            )
+            .click();
+          break;
+
         default:
           break;
       }
-      await this.driver.sleep(this.waitTime * 2);
+      await this.driver.sleep(10000);
       // 点订单
       try {
         await this.driver.findElement(By.css('#menu_item_9 .flex')).click();
@@ -104,7 +126,7 @@ export class TickTokCross extends Run {
       await this.driver.sleep(this.waitTime);
 
       // 点全部
-      await this.driver
+      await this.+driver
         .findElement(
           By.css(
             'div.OrderTab__TabContainer-sc-2a4d1v-1.dRBLKs.flex.space-x-12 > div:nth-child(1) > div > div',
